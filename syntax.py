8cb4a38e41dce0e10632086268dcfb6e59e3ee02
Pauline Ann Bautista <89290130+paulineeannn@users.noreply.github.com>
2024-02-10 11:04:54 +0800

    merged

---
+++ syntax.py
@@ -0,0 +1,37 @@
+from production_rules import *
+from tokens import spaces
+import errors
+
+def main(token_list):
+
+    # List to store the identified syntax
+    invalid_list = []
+    formatted_token = ""
+    code = ""
+    line = 0
+    for x in range(len(token_list)):
+        valid = False
+
+        # check if the token of one line exist in the production rules
+        if token_list[x][0] == "NEWLINE":
+            line += 1
+            for statement in STATEMENTS_LIST:
+                if formatted_token in statement:
+                    valid = True
+
+            print(formatted_token)
+            if not valid and (code not in spaces and code != ""):
+                error_message = errors.find_error(formatted_token)
+                invalid_list.append((line, code, error_message))
+
+            formatted_token = ""  # reset
+            code = ""
+
+        # create a string containing token of one line
+        else:
+            format_token = f"<{token_list[x][2]}>"
+            formatted_token += format_token
+            code +=  f"{token_list[x][1]}"
+
+
+    return invalid_list
--- tokens.py
+++ tokens.py
@@ -1,3 +1,4 @@
+
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 digits = '0123456789'
@@ -6,32 +7,36 @@ special_chars = {
     "." : "PERIOD",
     "@" : "COMMENT_SYM_SINGLE",
     "@@" : "COMMENT_SYM_MULTI",
+    "'" : "SINGLE_QUOTE",
     "#" : "POUND",
-    "=": "EQUAL",
-    # '"' : "DOUBLE_QUOTE",
-    # "'" : "SINGLE_QUOTE",
     "," : "COMMA",
-    ":" : "COLON",
     ";" : "SEMICOLON",
     "|" : "PIPE",
     "\\" : "BACKSLASH",
-    "(" : "PARENTHESIS_L",
-    ")" : "PARENTHESIS_R",
-    "[" : "BRACKET_L",
-    "]" : "BRACKET_R",
-    "{": "BRACE_L",         # wala sa documentation
-    "}": "BRACE_R",         # wala sa documentation
     "_" : "UNDERSCORE",
     "~" : "TILDE",
     "&" : "AMPERSAND"
 }
 
+delimiters = {
+    ":": "COLON_DELIMITER",
+    "(": "PARENTHESIS_OPEN",
+    ")": "PARENTHESIS_CLOSE",
+    "[": "BRACKET_OPEN",
+    "]": "BRACKET_CLOSE",
+    "{": "BRACE_OPEN",
+    "}": "BRACE_CLOSE",
+}
+
 operators= {
-    "+=" : "ADD_ASSIGNMENT",
-    "-=" : "SUBTRACT_ASSIGNMENT",
-    "*=" : "MULTIPLICATION_ASSIGNMENT",
-    "/=" : "DIVISION_ASSIGNMENT",
-    "%=" : "MODULO_ASSIGNMENT",
+    "=": "ASS_OP",
+    "+=" : "ADD_ASS",
+    "-=" : "SUBTRACT_ASS",
+    "*=" : "MULTIPLY_ASS",
+    "/=" : "DIVIDE_ASS",
+    "%=" : "MODULO_ASS",
+    "++" : "INCREMENT",
+    "--" : "DECREMENT",
     "+": "PLUS",
     "-": "MINUS",
     "*": "MULTIPLY",
@@ -45,56 +50,84 @@ operators= {
     "!=": "NOT_EQUAL",
     ">" : "GREATER_THAN",
     "<" : "LESS_THAN",
-    ">=" : "GREATER_THAN_EQUAL",
-    "<=" : "LESS_THAN_EQUAL",
+    ">=" : "GREATER_THAN_EQ",
+    "<=" : "LESS_THAN_EQ",
 }
 
 keywords = {
-    "bool",
-    "def",
-    "pow",
-    "char",
-    "when",
-    "otherwise",
-    "loop",
-    "input",
-    "output",
-    "stop",
-    "jump",
-    "arithSeq",
-    "arithSer",
-    "geoSeq",
-    "geoSer",
-    "distance",
-    "slope",
-    "pythagorean",
-    "quadratic",
-    "force",
-    "work",
-    "acceleration",
-    "power",
-    "momentum",
-    "potential",
-    "kinetic",
-    "toInt",
-    "toDeci",
-    "toStr"
+    "def" : "DEF",
+    "when" : "WHEN",
+    "otherwise" : "OTHERWISE",
+    "loop" : "LOOP",
+    "to" : "TO",
+    "input" : "INPUT",
+    "output" : "OUTPUT",
+    "stop" : "STOP",
+    "jump" : "JUMP",
+    "arithSeq" : "ARITHSEQ",
+    "arithSer" : "ARITHSER",
+    "geoSeq" : "GEOSEQ",
+    "geoSer" : "GEOSER",
+    "distance" : "DISTANCE",
+    "slope" : "SLOPE",
+    "pythagorean" : "PYTHAGOREAN",
+    "quadratic" : "QUADRATIC",
+    "force" : "FORCE",
+    "work" : "WORK",
+    "acceleration" : "ACCELERATION",
+    "power" : "POWER",
+    "momentum" : "MOMENTUM",
+    "potential" : "POTENTIAL",
+    "kinetic" : "KINETIC",
+    "toInt" : "TOINT",
+    "toDeci" : "TODECI",
+    "toStr" : "TOSTR"
 }
 
 reserved_words = {
-    "true",     # magkaiba case na nasa docu (true nasa #4 tas True sa #3)
-    "false",
-    "main",
-    "cont"
+    "TRUE" : "BOOL_VALUE",
+    "FALSE" : "BOOL_VALUE",
+    "main" : "MAIN",
+    "cont" : "CONT"
 }
 
 data_type = {
-    "int" : "INTEGER",
-    "str" : "STRING",
-    "char" : "CHARACTER",
-    "deci" : "DECIMAL",
-    "bool" : "BOOLEAN"
+    "int" : "DT_INT",
+    "str" : "DT_STR",
+    "char" : "DT_CHAR",
+    "deci" : "DT_DECI",
+    "bool" : "DT_BOOL"
 }
 
-spaces = {' ', '\n', '\t'}
+noise_words = {
+    "ean",
+    "eger",
+    "mal",
+    "ing",
+    "acter",
+    "ault",
+    "inue"
+}
 
+full_words = {
+    "boolean",
+    "integer",
+    "decimal",
+    "string",
+    "character",
+    "default",
+    "continue"
+}
+
+constants = {
+    "pi" : "PI",
+    "accGrav" : "ACCGRAV",
+    "euler" : "EULER",
+    "goldenRatio" : "GOLDENRATIO"
+}
+
+spaces = {' ' : "WHITESPACE",
+          '\n' : "NEWLINE",
+          '\t' : "INDENT",
+          '        ' : "INDENT2",
+}
